bc96ca84a3f44f49e84e74e2c45a6cbac646cc92
Zan <Zan.Li-2@student.uts.edu.au>
2023-08-24 14:36:31 +1000

    get_data

--- Getdata.py
+++ Getdata.py
@@ -3,15 +3,18 @@ import pandas as pd
 import numpy as np
 from API import get_respone
 import json
+
+
 def get_data():
     json_string = get_respone().decode('utf-8')
     data = json.loads(json_string)
     results = data.get('results', [])
     data = []
     for result in results:
-        print(result)
+        data.append(result)
+    data = pd.DataFrame(data)
     return data
 
 
-if __name__ == "__main__":
-    print(get_data())
+
+print(get_data())
