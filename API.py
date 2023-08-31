a388c518e6e923067e57b637ec6303f7ddaeb092
Zan <Zan.Li-2@student.uts.edu.au>
2023-08-31 15:41:11 +1000

    awards_classification

--- API.py
+++ API.py
@@ -1,7 +1,8 @@
 import requests
 import urllib.request, json
 
-def get_respone(page, limit):
+
+def get_awards(page, limit):
     try:
         url = f"https://api.fwc.gov.au/api/v1/awards?page={page}&limit={limit}"
         hdr ={
@@ -14,21 +15,23 @@ def get_respone(page, limit):
         return response.read()
     except Exception as e:
         return e
-    
 
-def get_salary(awardCode, classificationID, page, limit):
+
+def get_classification(awardcode,page,limit):
     try:
-        url = f"https://api.fwc.gov.au/api/v1/awards/{awardCode}/classifications/{classificationID}/pay-rates?page={page}&limit={limit}"
+        url = f"https://api.fwc.gov.au/api/v1/awards/{awardcode}/classifications?page={page}&limit={limit}"
+
         hdr ={
+        # Request headers
         'Cache-Control': 'no-cache',
         'Ocp-Apim-Subscription-Key': '04d87071235c4a0589baf617a3ab5fa0',
         }
+
         req = urllib.request.Request(url, headers=hdr)
+
         req.get_method = lambda: 'GET'
         response = urllib.request.urlopen(req)
-        print(response.getcode())
         return response.read()
     except Exception as e:
         print(e)
 
-#get_salary(1,1,1,10)
--- Getdata.py
+++ Getdata.py
@@ -1,48 +1,54 @@
 # Create a new DataFrame to save the dataset
 import pandas as pd
 import numpy as np
-from API import get_respone, get_salary
+from API import get_awards, get_classification
 import json
+import os
 
-def awards(page,limit):
-    json_string = get_respone(page,limit).decode('utf-8')
-    data = json.loads(json_string)
-    results = data.get('results', [])
-    data = []
-    for result in results:
-        data.append(result)
-    data = pd.DataFrame(data)
-    return data
-
-
-
-def get_data(awardCode,classificationID,page,limit):
-    json_string = get_salary(awardCode,classificationID,page,limit).decode('utf-8')
-    data = json.loads(json_string)
-    results = data.get('results', [])
-    data = []
-    for result in results:
-        data.append(result)
-    data = pd.DataFrame(data)
-    return data
-
-
-
-a = awards(50,60)
-print(a.columns)
-
-#json_string = get_salary(1,1,1,10).decode('utf-8')
-
-def salary():
+# Because every table has different rows, I need to know the shape of the table.
+# And then I can use for to get the data.
+# Due to the limitation, I only can get 100 data of every page.
+def awards():
+    counts = json.loads(get_awards(1,1))["_meta"]["result_count"]
     temp = []
-    for j in range(30):
-        for i in range(j*500,j*500+500):
-            df_['awardFixedID'][i]
-            df_['classificationFixedID'][i]
-            temp.append(get_data(df_['awardFixedID'][i],df_['classificationFixedID'][i],1,1))
-        time.sleep(100)
-    time.sleep(100)   
-    for i in range(15000,15220):
-            df_['awardFixedID'][i]
-            df_['classificationFixedID'][i]
-            temp.append(get_data(df_['awardFixedID'][i],df_['classificationFixedID'][i],1,1))
+    for i in range(1,(counts//100)+2):
+        json_string = get_awards(i,100).decode('utf-8')
+        data = json.loads(json_string)
+        results = data.get('results', [])
+        for result in results:
+            temp.append(result)
+    temp = pd.DataFrame(temp)
+    temp.to_csv('./Files/awards.csv')
+    return temp
+
+
+# read awardid from awards.csv, if there is no awards.csv, creating it.
+def classification():
+    csv_file_path = "./Files/awards.csv"
+    flag = False
+    if os.path.exists(csv_file_path):   
+        df = pd.read_csv(csv_file_path)  
+        if "award_fixed_id" in df.columns:
+            flag = True
+    if flag == False:
+        awards()
+    flag = True
+    if flag == True:
+        temp = []
+        for i in df['award_fixed_id']:
+            counts = json.loads(get_classification(i,1,1))["_meta"]["result_count"]
+            for j in range(1,(counts//100+2)):          
+                json_string = get_classification(i,j,100).decode('utf-8')
+                data = json.loads(json_string)
+                results = data.get('results', [])
+                for result in results:
+                    temp.append(result)
+    temp = pd.DataFrame(temp)
+    temp.to_csv('./Files/classification.csv')
+    return temp
+
+# awards()
+# classification()
+
+ 
+
