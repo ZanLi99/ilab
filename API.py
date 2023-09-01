b208ef829d844d9365184c91cb30549d26385f9d
Zan <Zan.Li-2@student.uts.edu.au>
2023-09-01 23:14:20 +1000

    finished_get_data

--- API.py
+++ API.py
@@ -16,10 +16,13 @@ def get_awards(page, limit):
     except Exception as e:
         return e
 
-
-def get_classification(awardcode,page,limit):
+# allowance: wage-allowances
+# classification: classifications
+# penalty: penalties
+# ex_allowance: expense-allowances
+def get_data(awardcode, parameter, page, limit):
     try:
-        url = f"https://api.fwc.gov.au/api/v1/awards/{awardcode}/classifications?page={page}&limit={limit}"
+        url = f"https://api.fwc.gov.au/api/v1/awards/{awardcode}/{parameter}?page={page}&limit={limit}"
 
         hdr ={
         # Request headers
@@ -33,5 +36,77 @@ def get_classification(awardcode,page,limit):
         response = urllib.request.urlopen(req)
         return response.read()
     except Exception as e:
-        print(e)
+        return(e)
 
+
+# def get_classification(awardcode,page,limit):
+#     try:
+#         url = f"https://api.fwc.gov.au/api/v1/awards/{awardcode}/classifications?page={page}&limit={limit}"
+
+#         hdr ={
+#         # Request headers
+#         'Cache-Control': 'no-cache',
+#         'Ocp-Apim-Subscription-Key': '04d87071235c4a0589baf617a3ab5fa0',
+#         }
+
+#         req = urllib.request.Request(url, headers=hdr)
+
+#         req.get_method = lambda: 'GET'
+#         response = urllib.request.urlopen(req)
+#         return response.read()
+#     except Exception as e:
+#         return(e)
+
+# def get_penalty(awardcode,page,limit):
+#     try:
+#         url = f"https://api.fwc.gov.au/api/v1/awards/{awardcode}/penalties?page={page}&limit={limit}"
+
+#         hdr ={
+#         # Request headers
+#         'Cache-Control': 'no-cache',
+#         'Ocp-Apim-Subscription-Key': '04d87071235c4a0589baf617a3ab5fa0',
+#         }
+
+#         req = urllib.request.Request(url, headers=hdr)
+
+#         req.get_method = lambda: 'GET'
+#         response = urllib.request.urlopen(req)
+#         return response.read()
+#     except Exception as e:
+#         return(e)
+
+# def get_ex_allowance(awardcode,page,limit):
+#     try:
+#         url = "https://api.fwc.gov.au/api/v1/awards/{awardcode}/expense-allowances?page={page}&limit={limit}"
+
+#         hdr ={
+#         # Request headers
+#         'Cache-Control': 'no-cache',
+#         'Ocp-Apim-Subscription-Key': '04d87071235c4a0589baf617a3ab5fa0',
+#         }
+
+#         req = urllib.request.Request(url, headers=hdr)
+
+#         req.get_method = lambda: 'GET'
+#         response = urllib.request.urlopen(req)
+#         return response.read()
+#     except Exception as e:
+#         return(e)
+    
+# def allowance(awardcode,page,limit):
+#     try:
+#         url = f"https://api.fwc.gov.au/api/v1/awards/{awardcode}/wage-allowances?page={page}&limit={limit}"
+
+#         hdr ={
+#         # Request headers
+#         'Cache-Control': 'no-cache',
+#         'Ocp-Apim-Subscription-Key': '••••••••••••••••••••••••••••••••',
+#         }
+
+#         req = urllib.request.Request(url, headers=hdr)
+
+#         req.get_method = lambda: 'GET'
+#         response = urllib.request.urlopen(req)
+#         return response.read()
+#     except Exception as e:
+#         return(e)
