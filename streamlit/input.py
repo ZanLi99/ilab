fc5d072b413c24be834bc38dfcc63da6f9e05a87
Ruoranleo <Zan.Li-2@student.uts.edu.au>
2023-09-09 21:56:44 +1000

    select

--- streamlit/input.py
+++ streamlit/input.py
@@ -1,16 +1,15 @@
 import streamlit as st
 from st_session import initialize_st
 from model import filter_job
-from function import selectawards, input
+from function import input,select_class
 
 
     
 initialize_st()
 input()
-
 st.write(st.session_state['input'])
-
-selectawards()
-
-
+#selectawards()
+#st.write(st.session_state['current_award'])
 st.dataframe(filter_job())
+
+select_class()
--- streamlit/st_session.py
+++ streamlit/st_session.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 
 def initialize_st():
-    st.session_state['user_input'] = []
+    if 'user_input' not in st.session_state:
+        st.session_state['user_input'] = []
+    if 'current_class' not in st.session_state:
+        st.session_state['current_class'] = []
     st.session_state['awards'] = pd.read_csv('./streamlit/awards.csv')
     st.session_state['classification'] = pd.read_csv('./streamlit/classification.csv')
     st.session_state['classification'] = st.session_state['classification'].apply(lambda x: x.astype(str).str.lower() if x.dtype == "object" else x)
-
     st.session_state['selection_class'] = st.session_state['classification']['classification'].drop_duplicates()
