9f36be91cd00207bbda37e5389ad99d1a06150b4
Ruoranleo <Zan.Li-2@student.uts.edu.au>
2023-09-09 21:17:49 +1000

    input

--- streamlit/st_session.py
+++ streamlit/st_session.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 
 def initialize_st():
     st.session_state['user_input'] = []
+    st.session_state['awards'] = pd.read_csv('./streamlit/awards.csv')
+    st.session_state['classification'] = pd.read_csv('./streamlit/classification.csv')
+    st.session_state['classification'] = st.session_state['classification'].apply(lambda x: x.astype(str).str.lower() if x.dtype == "object" else x)
+
+    st.session_state['selection_class'] = st.session_state['classification']['classification'].drop_duplicates()
