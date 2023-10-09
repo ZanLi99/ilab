2bc329f635a05e9d1b77859d71eb4c039c3bb7b0
ZanLi99 <Zan.Li-2@student.uts.edu.au>
2023-10-09 19:30:05 +1100

    1

--- streamlit/main.py
+++ streamlit/main.py
@@ -4,6 +4,7 @@ from model import filter_job
 from function import select_class, select_rate_type, base_rate, calculate_penalty,overtime,get_holiday_df,chooseholiday,calculate_weekend,calculate_salary
 from input import inputjob,worktime,work_type,work_time_everyday,salary,choosecountry
 from PIL import Image
+import random
 
 from streamlit_chat import message
 
@@ -24,6 +25,19 @@ chooseholiday()
 calculate_weekend()
 calculate_penalty()
 calculate_salary()
+
+if 'number_list' not in st.session_state:
+    st.session_state.number_list = []
+
+number = st.number_input("input work hours", value=0)
+if st.button("add"):
+    st.session_state.number_list.append(number)
+
+# 显示数字列表
+st.write("list:", st.session_state.number_list)
+
+
+       
 select_class()
 
 
