bfc48850254232fcaf7e89e88dcec1e9ddc2fd20
ZanLi99 <Zan.Li-2@student.uts.edu.au>
2023-10-10 15:48:47 +1100

    2

--- streamlit/main.py
+++ streamlit/main.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 import pandas as pd
 from st_session import initialize_st
-from model import filter_job
 from function import select_class, select_rate_type, base_rate, calculate_penalty,overtime,get_holiday_df,chooseholiday,calculate_weekend,calculate_salary
 from input import inputjob,worktime,work_type,work_time_everyday,salary,choosecountry,part_time_input,salary_type
 from PIL import Image
