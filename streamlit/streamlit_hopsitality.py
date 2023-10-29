baa41492c475b87cc624bad9a58caea419001543
mazzy7887 <martinspacek1999@gmail.com>
2023-10-29 13:39:39 +1100

    update_from before

--- streamlit/streamlit_hopsitality.py
+++ streamlit/streamlit_hopsitality.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import datetime
 import pandas as pd
-
-#st.time_input(datetime.time)
+import matplotlib as plt
+import seaborn as sns
 st.set_page_config(page_title='WageCraft Hospitality Award')
 
 
@@ -56,8 +56,6 @@ Job_information = {
                         'level_4': ['employee who has completed an apprenticeship or passed the appropriate trade test or otherwise has the appropriate level of training to perform the work of a tradesperson in dry cleaning or tailoring or as a butler.',  Adult_minimum_rate_weekly['level_4']],
                         'level_5': [' employee who has the appropriate level of training, including a supervisory course, and has responsibility for the supervision, training and co-ordination of employees engaged in a housekeeping department',  Adult_minimum_rate_weekly['level_5']]},
 
-#to add front office!!!
-
 'admininistration': { 'level_1': ['who is required to perform basic clerical and routine office duties such as collating, filing, photocopying and delivering messages.',  Adult_minimum_rate_weekly['level_2']],
                      'level_2': ['employee who is engaged in general clerical or office duties, such as typing, filing, basic data entry and calculating functions.',  Adult_minimum_rate_weekly['level_3']],
                      'level_3': ['info_1',  Adult_minimum_rate_weekly['level_4']],
@@ -83,22 +81,63 @@ Job_information = {
 
 }                       
 }
+input_job_type = ['Casual', 'Part time', 'Full time']
+
+day_of_week_dict = {
+    0 : 'Monday',
+    1 : 'Tuesday',
+    2 : 'Wednesday',
+    3 : 'Thursday',
+    4 : 'Friday',
+    5 : 'Saturday',
+6 : 'Sunday',
+}
+
+
 
 stream = st.selectbox('What stream are you part of', employment_stream)
 job_type_2 = st.selectbox('Job Type',Job_information[stream] )
 st.write('This is some of what you are expected to do in this role')
 st.write(Job_information[stream][job_type_2][0])
-input_job_type = ['Casual', 'Part time', 'Full time']
 job_type = st.selectbox('What is your employment type', input_job_type)
 st.write(Eployee_type_info[job_type])
-st.subheader('Input Day of Work Information')
 age = st.number_input('How old are you in years')
-d = st.date_input("What is the date of the day you worked ", datetime.date(2023, 9, 18))
-time_1 = st.time_input('What time did you start work', step=3600, key='time_1')
-time_2 = st.time_input(
-    'What time did you finish that work', step=3600, key='time_2')
+st.subheader('Input Day of Work Information')
+d_1 = datetime.datetime.now() - datetime.timedelta(days=6)
+st.header(f'Input hours for {d_1.day}/{d_1.month}/{d_1.month} ({day_of_week_dict[d_1.weekday()]})')
+time_1_1 = st.time_input('What time did you start work', step=3600, key='time_1_1')
+time_1_2 = st.time_input('What time did you finish that work', step=3600, key='time_1_2')
 
-break_1 = st.checkbox('Did you have a break this shift', key = 'break_1')
+d_2 = datetime.datetime.now() - datetime.timedelta(days=5)
+st.header(f'Input hours for {d_2.day}/{d_2.month}/{d_2.month} ({day_of_week_dict[d_2.weekday()]})')
+time_2_1 = st.time_input('What time did you start work', step=3600, key='time_2_1')
+time_2_2 = st.time_input('What time did you finish that work', step=3600, key='time_2_2')
+
+
+d_3 = datetime.datetime.now() - datetime.timedelta(days=4)
+st.header(f'Input hours for {d_3.day}/{d_3.month}/{d_3.month} ({day_of_week_dict[d_3.weekday()]})')
+time_3_1 = st.time_input('What time did you start work', step=3600, key='time_3_1')
+time_3_2 = st.time_input('What time did you finish that work', step=3600, key='time_3_2')
+
+d_4 = datetime.datetime.now() - datetime.timedelta(days=3)
+st.header(f'Input hours for {d_4.day}/{d_4.month}/{d_4.month} ({day_of_week_dict[d_4.weekday()]})')
+time_4_1 = st.time_input('What time did you start work', step=3600, key='time_4_1')
+time_4_2 = st.time_input('What time did you finish that work', step=3600, key='time_4_2')
+
+d_5 = datetime.datetime.now() - datetime.timedelta(days=2)
+st.header(f'Input hours for {d_5.day}/{d_5.month}/{d_5.month} ({day_of_week_dict[d_5.weekday()]})')
+time_5_1 = st.time_input('What time did you start work', step=3600, key='time_5_1')
+time_5_2 = st.time_input('What time did you finish that work', step=3600, key='time_5_2')
+
+d_6 = datetime.datetime.now() - datetime.timedelta(days=1)
+st.header(f'Input hours for {d_6.day}/{d_6.month}/{d_6.month} ({day_of_week_dict[d_6.weekday()]})')
+time_6_1 = st.time_input('What time did you start work', step=3600, key='time_6_1')
+time_6_2 = st.time_input('What time did you finish that work', step=3600, key='time_6_2')
+
+d_7 = datetime.datetime.now() 
+st.header(f'Input hours for {d_7.day}/{d_7.month}/{d_7.month} ({day_of_week_dict[d_7.weekday()]})')
+time_7_1 = st.time_input('What time did you start work', step=3600, key='time_7_1')
+time_7_2 = st.time_input('What time did you finish that work', step=3600, key='time_7_2')
 
 def combine_time_seconds(time_1, date_1, time_2, date_2):
     date = datetime.datetime.combine(date_1, time_1)
@@ -106,7 +145,6 @@ def combine_time_seconds(time_1, date_1, time_2, date_2):
     date_diff = date_2 - date
     date_diff = date_diff.seconds
     return date_diff
-
 
 def combine_time_hours(time_1, date_1, time_2, date_2):
     date = datetime.datetime.combine(date_1, time_1)
@@ -116,7 +154,14 @@ def combine_time_hours(time_1, date_1, time_2, date_2):
     date_diff = date_diff/3600
     return date_diff
 
-test = combine_time_hours(time_1, d, time_2, d)
+hours_1 = combine_time_hours(time_1_1, d_1, time_1_2, d_1)
+hours_2 = combine_time_hours(time_2_1, d_2, time_2_2, d_2)
+hours_3 = combine_time_hours(time_3_1, d_3, time_3_2, d_3)
+hours_4 = combine_time_hours(time_4_1, d_4, time_4_2, d_4)
+hours_5 = combine_time_hours(time_5_1, d_5, time_5_2, d_5)
+hours_6 = combine_time_hours(time_6_1, d_6, time_6_2, d_6)
+hours_7 = combine_time_hours(time_7_1, d_7, time_7_2, d_7)
+
 
 def junior_employee_payment(age):
     percent_rate = int
@@ -132,53 +177,9 @@ def junior_employee_payment(age):
         percent_rate = 1
     return percent_rate
 
-def junior_employee_office_payment(age):
-    percent_rate = int
-    if age < 16:
-        percent_rate = 0.45
-    elif age < 17:
-        percent_rate = 0.55
-    elif age < 18:
-        percent_rate = 0.65
-    elif age < 19:
-        percent_rate = 0.75
-    elif age < 20:
-        percent_rate = 0.90
-    else:
-        percent_rate = 1
-    return percent_rate
-
-junior_employee_office_payment(17.8)
-flat_increase = []
-percent_increase = []
-
-total_hours = int
-def penelty_rate_hours(time_1, date_1, time_2, date_2):
-    initial_date = datetime.datetime.combine(date_1, time_1)
-    final_date = datetime.datetime.combine(date_2, time_2)
-    date_7am = datetime.datetime(
-        final_date.year, final_date.month, final_date.day, 7)
-    date_7pm = datetime.datetime(
-        final_date.year, final_date.month, final_date.day, 7)
-    len_hours = final_date - initial_date
-    len_hours = (len_hours.seconds)/3600
-
-    if initial_date.weekday() == 6:
-        percent_increase.append(0.75)
-    elif initial_date.weekday() == 5:
-        percent_increase.append(len_hours * 0.5)
-    elif initial_date < date_7am < final_date or final_date < date_7am:
-        flat_increase.append(len_hours * 3.93)
-    elif final_date > date_7am > initial_date or initial_date > date_7pm:
-        flat_increase.append(len_hours * 2.62)
-    else:
-        pass
-
-    penelty_rate_hours.total_hours = len_hours 
-
 break_taken = []
 
-def break_inputs():
+def break_inputs(penelty_rate_hours, break_1):
     if 5 < penelty_rate_hours.total_hours < 6 & break_1 == True:
         st.write('For a shift between 5-6 hours long you are entitled to a  30 minute unpaid meal break')
     if 5 < penelty_rate_hours.total_hours < 6 & break_1 == False:
@@ -196,31 +197,73 @@ def break_inputs():
     if penelty_rate_hours.total_hours > 10 & break_1 == False:
         st.write('for a shift between 8-10 hours long you are entitled to a 30 minute unpaid meal break between the 2nd hour and 6th hour of the shift 2 20 minute paid breaks')
 
-penelty_rate_hours(time_1, d, time_2, d)
-break_inputs()
 
-age_penelty = junior_employee_payment(age)
-test = age_penelty * test
-base_res = (test * Job_information[stream][job_type_2][1])/38
-fixed_overtime = sum(flat_increase)
-var_overtime = sum(percent_increase) * (Job_information[stream][job_type_2][1])/38 * test
-casual_rate = 0
-if job_type == 'Casual':
-    casual_rate = 0.25
-casual_bonus = int(casual_rate * (Job_information[stream][job_type_2][1])/38 * test)
-total_res = base_res + fixed_overtime + var_overtime + casual_bonus
-overtime_sum = int(fixed_overtime + var_overtime)
-st.subheader('Payment Results')
 
-test_res = int(total_res)
-base_res = int(base_res)
-df_list = [[base_res, 'base pay'], [fixed_overtime, 'fixed overtime'], [var_overtime, 'var overtime'], [casual_bonus, 'casual bonus']]
-df = pd.DataFrame(df_list) 
 
-st.write('you should have earned a minimum of ',  f'${test_res}', 'over this time period' )
-st.write('This is made up of the combination of ', f'${base_res}' ,'in base pay and ' f'{overtime_sum}' ,'in overtime pay')
-st.write('You are receiving ', f'${casual_bonus}' ' for being a casual employee')
-df.columns = ['pay', 'type of bonus']
-df = df[df.pay > 0]
-st.bar_chart(data=df, y='pay', x='type of bonus')
-st.write('test')
+
+columns_2 = ['Day', 'Hours Worked', 'Base wage',  'Hourly pay', 'Overtime Bonus', 'Casual Bonus', 'Total Pay']
+def get_payment(hours, time_1, d_1, time_2, d_2):
+    flat_increase = []
+    percent_increase = []
+    total_hours = 0
+    day_of_week = day_of_week_dict[d_1.weekday()]
+    initial_date = datetime.datetime.combine(d_1, time_1)
+    final_date = datetime.datetime.combine(d_2, time_2)
+    date_7am = datetime.datetime(
+        final_date.year, final_date.month, final_date.day, 7)
+    date_7pm = datetime.datetime(
+        final_date.year, final_date.month, final_date.day, 7)
+    len_hours = final_date - initial_date
+    len_hours = (len_hours.seconds)/3600
+    if initial_date.weekday() == 6:
+        percent_increase.append(0.75)
+    elif initial_date.weekday() == 5:
+        percent_increase.append(len_hours * 0.5)
+    elif initial_date < date_7am < final_date or final_date < date_7am:
+        flat_increase.append(len_hours * 3.93)
+    elif final_date > date_7am > initial_date or initial_date > date_7pm:
+        flat_increase.append(len_hours * 2.62)
+    else:
+        pass
+    total_hours = len_hours 
+    age_penalty = junior_employee_payment(age)
+    pay = age_penalty * Job_information[stream][job_type_2][1]/38
+    hours = age_penalty * len_hours
+    base_res = (hours * Job_information[stream][job_type_2][1])/38
+    fixed_overtime = sum(flat_increase)
+    var_overtime = sum(percent_increase) * (Job_information[stream][job_type_2][1])/38 * hours
+    base_pay = (Job_information[stream][job_type_2][1])/38
+    casual_rate = 0
+    if job_type == 'Casual':
+        casual_rate = 0.25
+    casual_bonus = round(casual_rate * (Job_information[stream][job_type_2][1])/38 * hours,2)
+    total_res = base_res + fixed_overtime + var_overtime + casual_bonus
+    overtime_sum = round(fixed_overtime + var_overtime, 2)
+    df_list = [day_of_week, total_hours ,pay, base_res, overtime_sum,casual_bonus, total_res]
+    df = pd.DataFrame(df_list).T
+    return df
+
+df_1 = get_payment(hours_1, time_1_1, d_1, time_1_2, d_1)
+df_2 = get_payment(hours_2, time_2_1, d_2, time_2_2, d_2)
+df_3 = get_payment(hours_3, time_3_1, d_3, time_3_2, d_3)
+df_4 = get_payment(hours_4, time_4_1, d_4, time_4_2, d_4)
+df_5 = get_payment(hours_5, time_5_1, d_5, time_5_2, d_5)
+df_6 = get_payment(hours_6, time_6_1, d_6, time_6_2, d_6)
+df_7 = get_payment(hours_7, time_7_1, d_7, time_7_2, d_7)
+df = pd.concat([df_1, df_2, df_3, df_4, df_5, df_6, df_7])
+df.columns = columns_2
+
+st.write(f'In total you should have earnt {round(df["Hourly pay"].sum(),2)} with broken down in the following table')
+st.write(df)
+
+columns_2 = ['Day', 'Hours Worked', 'Base Pay',  'Total Pay', 'Overtime Bonus', 'Casual Bonus']
+
+fig = sns.barplot(df, x = 'Total Pay', y = 'Day')
+st.pyplot(fig.figure)
+
+
+ls = [[df['Total Pay'].sum(), 'total pay'], [df['Overtime Bonus'].sum(), 'Overtime Bonus'], [df['Casual Bonus'].sum(), 'Casual Bonus']]
+df_2 = pd.DataFrame(ls)
+df_2.columns = ['res', 'types']
+fig = sns.barplot(df_2, x = 'res', y = 'types')
+st.pyplot(fig.figure)
