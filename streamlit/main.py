c62a97a6217a9b64804c560297e48d2d34d13688
mazzy7887 <martinspacek1999@gmail.com>
2023-10-30 13:24:53 +1100

    final_update
    final_update

--- streamlit/main.py
+++ streamlit/main.py
@@ -47,7 +47,7 @@ elif page == "Page 2":
 
 
 st.header('WageCraft Hospitality Wage Calculator', divider='gray')
-st.subheader('Some of the Industries covered by this award include', help = 'To help find out what industry you are in use [This link](https://www.fairwork.gov.au/employment-conditions/awards/awards-summary/ma000009-summary)')
+st.subheader('Some of the Industries covered by this award include', help = 'To help find out what industry you are in use [Fair Work sumamry](https://www.fairwork.gov.au/employment-conditions/awards/awards-summary/ma000009-summary)')
 st.write('- Tourist accomodations')
 st.write('- restaurants and convention facilities')
 st.write('- nightclubs function areas')
@@ -71,53 +71,53 @@ Eployee_type_info = {
 
 }
 
-employment_stream = ['Food and beverage', 'kitchen', 'guest services stream', 'admininistration', 'security', 'leisure activities stream',  'maintenance and trades']
+employment_stream = ['Food and beverage', 'Kitchen', 'Guest Service', 'Admininistration', 'Security', 'Leisure Activities Stream',  'Stores Stream','Maintenance and Trades' ]
 
 Job_information = {
 'Food and beverage': {                     
-                        'level 1': ['picking up glasses, emptying ashtrays,removing food plates,setting and wiping down tables, cleaning and tidying associated areas  ', Adult_minimum_rate_weekly['level_1']],
-                        'level 2': ['handling liquor, assisting in the bottle department, waiting duties, recieving money, taking reservations, greeting and seating guessts', Adult_minimum_rate_weekly['level_2']], 
-                        'level 3': ['operating a mechanical lifting device,attending a wagering terminal, electronic gaming terminal or similar terminal, mixing drinks, training staff of a lower grade, supervising staff of a lower grade ', Adult_minimum_rate_weekly['level_3']],
-                        'level 4': ['completed an apprenticeshop or passed trade tests to do specialised duties', Adult_minimum_rate_weekly['level_4']],
+                        'level 1': ['Picking up glasses, emptying ashtrays, removing food plates, setting and wiping down tables, cleaning and tidying associated areas', Adult_minimum_rate_weekly['level_1']],
+                        'level 2': ['Handling liquor, assisting in the bottle department, waiting duties, recieving money, taking reservations, greeting and seating guessts', Adult_minimum_rate_weekly['level_2']], 
+                        'level 3': ['Operating a mechanical lifting device, attending a wagering terminal, electronic gaming terminal or similar terminal, mixing drinks, training staff of a lower grade, supervising staff of a lower grade ', Adult_minimum_rate_weekly['level_3']],
+                        'level 4': ['Completed an apprenticeshop or passed trade tests to do specialised duties', Adult_minimum_rate_weekly['level_4']],
                         'level 5': ['Have appropriate training including supervisory and has the responsiblity of training, co-ordinating and supervising staff', Adult_minimum_rate_weekly['level_5']]},
-'Kitchen' :{'Kitchen attendant grade 1 ': ['general cleaning duties or food preperation, assisting employyes that are cooking, assembling ingredients, general pantry duties',  Adult_minimum_rate_weekly['level_1']],
-            'Kitchen attendant grade 2 ': ['who has the appropriate level of training, and who is engaged in specialised non-cooking duties in a kitchen or food preparation area or in supervising kitchen attendants.',  Adult_minimum_rate_weekly['level_2']],
-            'Kitchen attendant grade 3': [' who has the appropriate level of training, including a supervisory course, and has responsibility for the supervision, training and co-ordination of kitchen attendants of a lower classification.',  Adult_minimum_rate_weekly['level_3']],
+'Kitchen' :{'Kitchen attendant grade 1 ': ['General cleaning duties or food preperation, assisting employees that are cooking, assembling ingredients, general pantry duties',  Adult_minimum_rate_weekly['level_1']],
+            'Kitchen attendant grade 2 ': ['Who has the appropriate level of training, and who is engaged in specialised non-cooking duties in a kitchen or food preparation area or in supervising kitchen attendants.',  Adult_minimum_rate_weekly['level_2']],
+            'Kitchen attendant grade 3': [' Who has the appropriate level of training, including a supervisory course, and has responsibility for the supervision, training and co-ordination of kitchen attendants of a lower classification.',  Adult_minimum_rate_weekly['level_3']],
             'Cook grade 1 ': ['who is engaged in cooking breakfasts and snacks, baking, pastry cooking or butchering.',  Adult_minimum_rate_weekly['level_2']],
-            'Cook grade 2 ': ['employee who has the appropriate level of training and who performs cooking duties such as baking, pastry cooking or butchering.',  Adult_minimum_rate_weekly['level_3']],
-            'Cook grade 3 (tradesperson)  ': ['commi chef or equivalent who has completed an apprenticeship or passed the appropriate trade test and who is engaged in cooking, baking, pastry cooking or butchering duties.',  Adult_minimum_rate_weekly['level_4']],
-            'Cook grade 4 (tradesperson) ': ['demi chef or equivalent who has completed an apprenticeship or passed the appropriate trade test and who is engaged to perform general or specialised cooking, butchering, baking or pastry cooking duties or supervises and trains other cooks and kitchen employees.',  Adult_minimum_rate_weekly['level_5']],
-            'Cook grade 5 (tradesperson) ': ['chef de partie or equivalent who has completed an apprenticeship or passed the appropriate trade test in cooking, butchering, baking or pastry cooking and who performs any of the following:, general and specialised duties, including supervision or training of kitchen employees',  Adult_minimum_rate_weekly['level_6']],
+            'Cook grade 2 ': ['Employee who has the appropriate level of training and who performs cooking duties such as baking, pastry cooking or butchering.',  Adult_minimum_rate_weekly['level_3']],
+            'Cook grade 3 (tradesperson)  ': ['Commi chef or equivalent who has completed an apprenticeship or passed the appropriate trade test and who is engaged in cooking, baking, pastry cooking or butchering duties.',  Adult_minimum_rate_weekly['level_4']],
+            'Cook grade 4 (tradesperson) ': ['Demi chef or equivalent who has completed an apprenticeship or passed the appropriate trade test and who is engaged to perform general or specialised cooking, butchering, baking or pastry cooking duties or supervises and trains other cooks and kitchen employees.',  Adult_minimum_rate_weekly['level_5']],
+            'Cook grade 5 (tradesperson) ': ['Chef de partie or equivalent who has completed an apprenticeship or passed the appropriate trade test in cooking, butchering, baking or pastry cooking and who performs any of the following:, general and specialised duties, including supervision or training of kitchen employees',  Adult_minimum_rate_weekly['level_6']],
               },
 
-'Guest Service': { 'level 1': ['laundry duties, repairs to clothing, collecting laundry, general cleaning, parking motor vehicles',  Adult_minimum_rate_weekly['level_1']],
-                        'level 2': ['cleaning/servicing accomodation areas, receiving and assisting guests at entrance, driving , transfering baggage, cleaning, bulter services',  Adult_minimum_rate_weekly['level_2']],
-                        'level 3': ['supervising staff, butler services, major repairs, dry lceaning',  Adult_minimum_rate_weekly['level_3']],
-                        'level 4': ['employee who has completed an apprenticeship or passed the appropriate trade test or otherwise has the appropriate level of training to perform the work of a tradesperson in dry cleaning or tailoring or as a butler.',  Adult_minimum_rate_weekly['level_4']],
-                        'level 5': [' employee who has the appropriate level of training, including a supervisory course, and has responsibility for the supervision, training and co-ordination of employees engaged in a housekeeping department',  Adult_minimum_rate_weekly['level_5']]},
+'Guest Service': { 'level 1': ['Laundry duties, repairs to clothing, collecting laundry, general cleaning, parking motor vehicles',  Adult_minimum_rate_weekly['level_1']],
+                        'level 2': ['Cleaning/servicing accomodation areas, receiving and assisting guests at entrance, driving , transfering baggage, cleaning, bulter services',  Adult_minimum_rate_weekly['level_2']],
+                        'level 3': ['Supervising staff, butler services, major repairs, dry lceaning',  Adult_minimum_rate_weekly['level_3']],
+                        'level 4': ['Employee who has completed an apprenticeship or passed the appropriate trade test or otherwise has the appropriate level of training to perform the work of a tradesperson in dry cleaning or tailoring or as a butler.',  Adult_minimum_rate_weekly['level_4']],
+                        'level 5': ['Employee who has the appropriate level of training, including a supervisory course, and has responsibility for the supervision, training and co-ordination of employees engaged in a housekeeping department',  Adult_minimum_rate_weekly['level_5']]},
 
-'Admininistration': { 'level 1': ['who is required to perform basic clerical and routine office duties such as collating, filing, photocopying and delivering messages.',  Adult_minimum_rate_weekly['level_2']],
-                     'level 2': ['employee who is engaged in general clerical or office duties, such as typing, filing, basic data entry and calculating functions.',  Adult_minimum_rate_weekly['level_3']],
-                     'level 3': ['info_1',  Adult_minimum_rate_weekly['level_4']],
-                    'Clerical supervisor': [' employee who has the appropriate level of training, including a supervisory course, and who co-ordinates other clerical staff.',  Adult_minimum_rate_weekly['level_5']], },
+'Admininistration': { 'Clerical level 1': ['Who is required to perform basic clerical and routine office duties such as collating, filing, photocopying and delivering messages.',  Adult_minimum_rate_weekly['level_2']],
+                     'Clerical level 2': ['Employee who is engaged in general clerical or office duties, such as typing, filing, basic data entry and calculating functions.',  Adult_minimum_rate_weekly['level_3']],
+                     'Clerical level 3': ['Has the appropriate level sof training, retrieves data through a computer terminal, keeps appropriate records, maintains mail register and records',  Adult_minimum_rate_weekly['level_4']],
+                    'Clerical supervisor': ['Employee who has the appropriate level of training, including a supervisory course, and who co-ordinates other clerical staff.',  Adult_minimum_rate_weekly['level_5']], },
 
-'Security': { 'Door person/security officer grade 1': ['who assists in the maintenance of dress standards and good order at an establishment.',  Adult_minimum_rate_weekly['level_2']],
-              'Timekeeper/security officer grade 2': ['person who is responsible for the timekeeping of employees, for the security of keys, for the checking in and out of delivery vehicles or the supervision of doorperson/security officer grade 1 employees',  Adult_minimum_rate_weekly['level_3']] },
+'Security': { 'Door person/security officer grade 1': ['Who assists in the maintenance of dress standards and good order at an establishment.',  Adult_minimum_rate_weekly['level_2']],
+              'Timekeeper/security officer grade 2': ['Person who is responsible for the timekeeping of employees, for the security of keys, for the checking in and out of delivery vehicles or the supervision of doorperson/security officer grade 1 employees',  Adult_minimum_rate_weekly['level_3']] },
 
-'Leisure Activities Stream': { 'Leisure attendant grade 1;': [' person who acts as an assistant instructor or pool attendant or is responsible for the setting up, distribution and care of equipment and the taking of bookings.',  Adult_minimum_rate_weekly['level_2']],
-                                'Leisure attendant grade 2': [' person who has the appropriate level of training and takes classes or directs leisure activities such as sporting areas, health clubs and swimming pool',  Adult_minimum_rate_weekly['level_3']],
-                                'Leisure attendant grade 3': [' a person who has the appropriate level of training and who plans and co-ordinates leisure activities for guests and may supervise other leisure attendant',  Adult_minimum_rate_weekly['level_4']], },
+'Leisure Activities Stream': { 'Leisure attendant grade 1;': ['Person who acts as an assistant instructor or pool attendant or is responsible for the setting up, distribution and care of equipment and the taking of bookings.',  Adult_minimum_rate_weekly['level_2']],
+                                'Leisure attendant grade 2': ['Person who has the appropriate level of training and takes classes or directs leisure activities such as sporting areas, health clubs and swimming pool',  Adult_minimum_rate_weekly['level_3']],
+                                'Leisure attendant grade 3': ['Person who has the appropriate level of training and who plans and co-ordinates leisure activities for guests and may supervise other leisure attendant',  Adult_minimum_rate_weekly['level_4']], },
 
-'Stores Stream': { 'Storeperson grade 1': ['employee who receives and stores general and perishable goods and cleans the store area.',  Adult_minimum_rate_weekly['level_2']],
-                            'Storeperson grade 2': ['employee who, in addition to the duties for a storeperson grade 1, may also operate mechanical lifting equipment such as a fork-lift or who may perform duties of a more complex nature',  Adult_minimum_rate_weekly['level_3']],
-                            'Storeperson grade 3;': ['appropriate levels of training',  Adult_minimum_rate_weekly['level_4']],},
+'Stores Stream': { 'Storeperson grade 1': ['Employee who receives and stores general and perishable goods and cleans the store area.',  Adult_minimum_rate_weekly['level_2']],
+                            'Storeperson grade 2': ['Employee who, in addition to the duties for a storeperson grade 1, may also operate mechanical lifting equipment such as a fork-lift or who may perform duties of a more complex nature',  Adult_minimum_rate_weekly['level_3']],
+                            'Storeperson grade 3;': ['Appropriate levels of training, implements quality control techinuqes or precedures, responsible for a store or warehouse area, supervise and gives directions to employess. ',  Adult_minimum_rate_weekly['level_4']],},
 
 'Maintenance and Trades': { 'Handyperson': ['who is not a tradesperson and whose duties include performing routine repair work and maintenance in and about the employer’s premises.',  Adult_minimum_rate_weekly['level_3']],
-                             'Fork-lift driver': ['employee who has a recognised fork-lift licence and who is engaged solely to drive a fork-lift vehicle',  Adult_minimum_rate_weekly['level_3']],
-                             'Gardener grade 1': ['keeping areas tidy, weeding/watering, trimming, assisting in prepareing areas for play, assisting in green maintinebce/construction, operating a limited range of vehicles',  Adult_minimum_rate_weekly['level_2']],
-                             'Gardener grade 2': ['handyperson, supervising, completing records, maintinence for turn, ',  Adult_minimum_rate_weekly['level_3']],
-                             'Gardener grade 3 (tradesperson)': ['operating machinery, supervision of employyes, training apprentices, applying fertiliser, fungicides, herbicides, insectisides',  Adult_minimum_rate_weekly['level_4']],
-                             'Gardener grade 4 (tradesperson)': ['has the training for grade 3 and is supervising tradespersons, writing reports, general liason with management, performing specialist skills',  Adult_minimum_rate_weekly['level_5']],
+                             'Fork-lift driver': ['Employee who has a recognised fork-lift licence and who is engaged solely to drive a fork-lift vehicle',  Adult_minimum_rate_weekly['level_3']],
+                             'Gardener grade 1': ['Keeping areas tidy, weeding/watering, trimming, assisting in prepareing areas for play, assisting in green maintinebce/construction, operating a limited range of vehicles',  Adult_minimum_rate_weekly['level_2']],
+                             'Gardener grade 2': ['Handyperson, supervising, completing records, maintinence for turn, ',  Adult_minimum_rate_weekly['level_3']],
+                             'Gardener grade 3 (tradesperson)': ['Operating machinery, supervision of employyes, training apprentices, applying fertiliser, fungicides, herbicides, insectisides',  Adult_minimum_rate_weekly['level_4']],
+                             'Gardener grade 4 (tradesperson)': ['Has the training for grade 3 and is supervising tradespersons, writing reports, general liason with management, performing specialist skills',  Adult_minimum_rate_weekly['level_5']],
 
 }                       
 }
@@ -136,9 +136,10 @@ day_of_week_dict = {
 
 
 stream = st.selectbox('What part of hospitality do you work in', employment_stream)
-job_type_2 = st.selectbox('Which of the following best describes your position',Job_information[stream] , help='For more information go to [link](https://library.fairwork.gov.au/award/?krn=MA000009) ')
-st.write('This is some of your job expectations')
-st.write(Job_information[stream][job_type_2][0])
+job_type_2 = st.selectbox('Which of the following best describes your position',Job_information[stream] , help='For more information go to [Award](https://library.fairwork.gov.au/award/?krn=MA000009) ')
+with st.expander('This is some of your job expectations'):
+    st.write(Job_information[stream][job_type_2][0])
+
 job_type = st.selectbox('What is your employment type', input_job_type)
 st.write(Eployee_type_info[job_type])
 age = st.number_input('Enter your age in years')
